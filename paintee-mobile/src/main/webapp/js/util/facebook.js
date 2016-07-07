a8c7eb8578294721274e4b75e51765a8ca4601d2
Yoon ji won <jiwoniy@gmail.com>
2016-07-07 22:56:19 +0900

    statusChangeCallback이 없어도 됨.... 우리는 쓰지 않는다.

--- paintee-mobile/src/main/webapp/js/util/facebook.js
+++ paintee-mobile/src/main/webapp/js/util/facebook.js
@@ -8,17 +8,36 @@ window.fbAsyncInit = function() {
 		xfbml      : true,  // parse social plugins on this page
 		version    : 'v2.5' // use graph api version 2.5
 	});
+	
+	FB.getLoginStatus(function(response) {
+		//console.log('언제...');
+		//statusChangeCallback(response);
+	});
 
 	openFB.init({appId: '235488130138448'});
+	
+	/*
+	function statusChangeCallback(response) {
+	    console.log('statusChangeCallback');
+	    
+	    // The response object is returned with a status field that lets the
+	    // app know the current login status of the person.
+	    // Full docs on the response object can be found in the documentation
+	    // for FB.getLoginStatus().
+	    if (response.status === 'connected') {
+	      // Logged into your app and Facebook.
+	    	console.log('connected');
+	    } else if (response.status === 'not_authorized') {
+	      // The person is logged into Facebook, but not your app.
+	    	console.log('not_authorized');
+	    } else {
+	      // The person is not logged into Facebook, so we're not sure if
+	      // they are logged into this app or not.
+	     
+	    }
+	}
+	*/
 
-	FB.getLoginStatus(function(response) {
-		
-		
-//			console.log(response);
-//			console.log('test');
-		
-			statusChangeCallback(response);
-		});
 
 		// FB.getLoginStatus(function(response) {
 		// 		 if (response.status === 'connected') {
@@ -40,13 +59,11 @@ window.fbAsyncInit = function() {
 	//    your app or not.
 	//
 	// These three cases are handled in the callback function.
-/*
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});*/
 };
 
-// Load the SDK asynchronously
+
+
+//Load the SDK asynchronously
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
