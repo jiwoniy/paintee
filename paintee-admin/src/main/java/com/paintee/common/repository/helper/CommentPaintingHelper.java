30cf402da395ef0c46a71b31e4ca7584439ad897
javadori <javahaja@hotmail.com>
2016-10-09 21:41:51 +0900

    no message

---
+++ paintee-admin/src/main/java/com/paintee/common/repository/helper/CommentPaintingHelper.java
@@ -0,0 +1,22 @@
+package com.paintee.common.repository.helper;
+
+import com.paintee.common.repository.mapper.CommentPaintingMapper;
+
+/**
+@class CommentPaintingHelper
+com.paintee.common.repository.helper \n
+   ㄴ ConfirmHashHelper.java
+ @section 클래스작성정보
+    |    항  목       |      내  용       |
+    | :-------------: | -------------   |
+    | Company | Paintee |
+    | Author | Administrator |
+    | Date | 2016. 10. 9. 오후 5:41:47 |
+    | Class Version | v1.0 |
+    | 작업자 | Administrator |
+ @section 상세설명
+ - comment 정보 helper
+*/
+public interface CommentPaintingHelper extends CommentPaintingMapper {
+
+}
